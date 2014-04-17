03645d976c00aa8b4042715771ba12beb7d1316f
Pete Sellars <psellars@gmail.com>
2014-04-17 22:38:05 +1200

    Added RSpec-Book required gems and added Puppet gem due to use of git source in Puppetfile Added RSpec gem to default node Fixed syntax issues to keep IDEA happy!
    Added cucumber gem for use with RSpec book code
    Added puppet gem for use when running librarian-puppet with git sources

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,8 +1,11 @@
 node default {
 
-  class { 'rbenv': }
+  include rbenv
 
   rbenv::plugin { [ 'sstephenson/rbenv-vars', 'sstephenson/ruby-build' ]: }
   rbenv::build { '1.9.3-p545': global => true }
+  rbenv::gem { 'puppet' : ruby_version => '1.9.3-p545' }
+  rbenv::gem { 'rspec' : ruby_version => '1.9.3-p545' }
+  rbenv::gem { 'cucumber' : ruby_version => '1.9.3-p545' }
 
 }
